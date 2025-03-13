cb03dbca5b2fc4941853491a23d26efd05d6509b
sanahashim <sanahashim33@outlook.com>
2025-03-13 17:10:54 -0400

    Update script.js
    Updated variable names to match

--- script.js
+++ script.js
@@ -2,24 +2,47 @@
 mapboxgl.accessToken = 'pk.eyJ1Ijoic2FuYWhhc2hpbSIsImEiOiJjbTV5NnpreTcwMTBqMnJvN2ltN2Jjcmo1In0.JpuPVI1IOhbjC1mjNPi5YQ'; 
 
 // Initialize the first map (CanFed Map)
-var map1 = new mapboxgl.Map({
-    container: 'map1', 
-    style: 'mapbox://styles/mapbox/streets-v12', 
-    center: [-79.640579, 43.595310], // Location [lon, lat] (Mississauga)
-    zoom: 10 
+const map_can_fed = new mapboxgl.Map({
+    container: "can-fed-map",
+    style: 'mapbox://styles/faiza132/cm72f283a007a01quayc2g04v',
+    center: [-79.3878583, 43.7205208], // Location [lon, lat] (Mississauga)
+    zoom: 12
 });
 
 // 1st Map Navigation Controls
-map1.addControl(new mapboxgl.NavigationControl());
+map_can_fed.addControl(new mapboxgl.NavigationControl());
+
+map_can_fed.on('load', () => {
+    map_can_fed.addSource('transit_data', {
+        'type': 'vector',
+        'url': "mapbox://faiza132.6pdcyj1q"
+    });
+    //Layer with symbology
+    map_can_fed.addLayer({
+        'id': 'transit_buffers',
+        'type': 'circle',
+        'source': 'transit_data',
+        'minzoom': 0,
+        'maxzoom': 24,
+        'paint': {
+            'circle-color': "#f77f00",
+            'circle-opacity': 0.9,
+            'circle-stroke-color': 'black',
+            'circle-radius': 6
+        },
+        'source-layer': "transit_data-9938bw"
+
+    });
+});
 
 // 1st Map Pop-Up
 new mapboxgl.Marker()
     .setLngLat([-79.640579, 43.595310])
     .setPopup(new mapboxgl.Popup().setHTML('<p>Mississauga: High Food Insecurity</p>'))
-    .addTo(map1);
+    .addTo(map_can_fed);
 
 // Initialize the second map (Demographics Map)
-var map2 = new mapboxgl.Map({
+var map_dem = new mapboxgl.Map({
     container: 'map2', 
     style: 'mapbox://styles/mapbox/light-v11', 
     center: [-79.640579, 43.595310], // Location [lon, lat] (Mississauga)
@@ -27,10 +50,14 @@ var map2 = new mapboxgl.Map({
 });
 
 // 2nd Map Navigation Controls
-map2.addControl(new mapboxgl.NavigationControl());
+map_dem.addControl(new mapboxgl.NavigationControl());
 
 // 2nd Map Pop-Up
 new mapboxgl.Marker()
     .setLngLat([-79.640579, 43.595310])
     .setPopup(new mapboxgl.Popup().setHTML('<p>Mississauga: High Population Density</p>'))
-    .addTo(map2);
+    .addTo(map_dem);
+
+
+
+    
